41c8245b366a20c18406ded7e5bf548bebb46401
AnthonyLupica <arl127@uakron.edu>
2024-04-12 16:59:29 -0400

    Restructure file layout

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import './App.css';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 const App = () => {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
@@ -17,7 +17,7 @@ const App = () => {
     <div>
       <Header as="h1" icon="users" content="Reactivities" />
       <List>
-        {activities.map((activity: any) => {
+        {activities.map((activity) => {
           return (
             <List.Item key={activity.id}>
               {activity.title}
