b9bf12bd846613d88db65fa412fe9f22de4ed347
AnthonyLupica <arl127@uakron.edu>
2024-05-25 12:55:33 -0400

    Revert "Create Journal Entity and Seeding"
    This reverts commit ee7c77af38d2c8806443184866a3a368fe63d492.

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -5,20 +5,10 @@ namespace Persistence
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) : base(options) {}
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
 
         public DbSet<Activity> Activities { get; set; }
-
-        public DbSet<Journal> Journals { get; set; }
-
-        public DbSet<EmotionScore> EmotionScores { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Journal>()
-                .HasOne(j => j.EmotionScore)
-                .WithOne(e => e.Journal)
-                .HasForeignKey<EmotionScore>(c => c.FkJournal);
-        }
     }
 }
